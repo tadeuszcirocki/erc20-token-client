6c3175e89aa05f579fcad1acd1ea97864f567636
Tadeusz Cirocki <45399049+tadeuszcirocki@users.noreply.github.com>
2021-11-29 00:56:58 +0000

    true initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,22 @@ import './App.css';
 
 export default function App() {
 
-  
-  
 
 
 
 
   return (
     <div className="mainContainer">
+
       <div className="dataContainer">
         <div className="header">
         👋 Hey there!
         </div>
 
         <div className="bio">
-        text text text text text text text text
+        text text text text
         </div>
 
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
-        </button>
       </div>
     </div>
   );
